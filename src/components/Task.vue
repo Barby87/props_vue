5599d174f0e4a74f0f6d09525b379a5b9739f5c9
Barby87 <bcaceres.sepulveda@hotmail.com>
2020-03-27 00:51:11 -0300

    Creating list

---
+++ src/components/Task.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <form @submit.prevent="addTask">
+      <span>{{title}}</span>
+      <input v-model="newTask" placeholder="Ingresa nuevas tareas">
+      <button type="submit">Añadir</button>
+    </form>
+
+    <ul>
+      <li v-for="task in tasks" :key="task">
+        {{task}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Tarea:',
+      tasks: [],
+      newTask: ''
+    }
+  },
+  methods: {
+      addTask(e) {
+         this.tasks.push(this.newTask);
+            // Limpiar input luego de agregar tarea
+            this.newTask = '';
+      }
+    }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
